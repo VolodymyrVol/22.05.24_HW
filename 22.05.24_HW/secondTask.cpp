08f04f808dcc50859ec560256aba53c09f8838f9
Vova <lololoskafry@gmail.com>
2024-05-27 11:55:52 +0300

    final version of project

--- 22.05.24_HW/secondTask.cpp
+++ 22.05.24_HW/secondTask.cpp
@@ -13,7 +13,7 @@ public:
 		model = country = color = nullptr;
 		year = price = 0;
 	}
-	Car(const char* Model, const char* Country, const char* Color, int Year, double Price) // 2 params
+	Car(const char* Model, const char* Country, const char* Color, int Year, double Price) 
 	{
 		model = new char[strlen(Model) + 1];
 		strcpy_s(model, strlen(Model) + 1, Model);
@@ -34,13 +34,33 @@ public:
 		delete[] country;
 		delete[] color;
 	}
-	Init()
+	void Input(const char* Model, const char* Country, const char* Color, int Year, double Price)
 	{
+		model = new char[strlen(Model) + 1];
+		strcpy_s(model, strlen(Model) + 1, Model);
 
+		country = new char[strlen(Country) + 1];
+		strcpy_s(country, strlen(Country) + 1, Country);
+
+		color = new char[strlen(Color) + 1];
+		strcpy_s(color, strlen(Color) + 1, Color);
+
+		year = Year;
+
+		price = Price;
 	}
-	Show()
+	void Init()
 	{
-
+		model = country = color = nullptr;
+		year = price = 0;
+	}
+	void Show()
+	{
+		cout << "Model: " << model 
+			<< "\nCountry: " << country
+			<< "\nColor: " << color
+			<< "\nYear: " << year 
+			<< "\nPrice: " << price << endl;
 	}
 };
 
@@ -49,5 +69,6 @@ public:
 
 int main()
 {
-
+	Car a{"Nissan", "Japan", "Grey", 2007, 30000};
+	a.Show();
 }
